1fdf17d8b2d6411b2dd7f538411c9903d900949c
Nicholas Moeller <nwmoeller75@gmail.com>
2018-11-05 12:13:51 -0600

    1st commit

---
+++ src/main/java/org/o7planning/springmvcsecurity/dao/impl/UserInfoDAOImpl.java
@@ -0,0 +1,53 @@
+package org.o7planning.springmvcsecurity.dao.impl;
+
+import org.o7planning.springmvcsecurity.dao.UserInfoDAO;
+import org.o7planning.springmvcsecurity.mapper.UserInfoMapper;
+import org.o7planning.springmvcsecurity.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserInfoDAOImpl extends JdbcDaoSupport implements UserInfoDAO {
+
+    @Autowired
+    public UserInfoDAOImpl(DataSource dataSource) {
+        this.setDataSource(dataSource);
+    }
+
+
+    @Override
+    public UserInfo findUserInfo(String userName) {
+        String sql = "Select u.Username,u.Password "//
+                + " from Users u where u.Username = ? ";
+
+        Object[] params = new Object[] { userName };
+        UserInfoMapper mapper = new UserInfoMapper();
+        try {
+            UserInfo userInfo = this.getJdbcTemplate().queryForObject(sql, params, mapper);
+            return userInfo;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+
+    @Override
+    public List<String> getUserRoles(String userName) {
+        String sql = "Select r.User_Role "//
+                + " from User_Roles r where r.Username = ? ";
+
+        Object[] params = new Object[] { userName };
+
+        List<String> roles = this.getJdbcTemplate().queryForList(sql,params, String.class);
+
+        return roles;
+    }
+
+}
